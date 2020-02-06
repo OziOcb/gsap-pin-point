e41c60516ea89a1295d6ce00f8171546d4e4f896
”Ozi <31310132+OziOcb@users.noreply.github.com>
2020-02-06 22:13:15 +0000

    Master commit

---
+++ js/main.js
@@ -0,0 +1,33 @@
+window.addEventListener("DOMContentLoaded", event => {
+    let pin = document.querySelector(".pin");
+    let line = document.querySelector(".line");
+    let circle = document.querySelector(".circle");
+
+    let visible = false;
+    const tl = gsap.timeline({ paused: true });
+
+    tl.to(pin, 0.3, { scale: 0.9, transformOrigin: "50% 50%" })
+        .to(pin, 0.2, { scale: 1.2 })
+        .to(pin, 0.2, { scale: 1 })
+        .to(line, 1, { attr: { "stroke-dashoffset": 0 }, opacity: 1 }, "-=.4")
+        .to(
+            circle,
+            2,
+            {
+                attr: { "stroke-dashoffset": 0 },
+                opacity: 1,
+                ease: "power4"
+            },
+            "-=0.5"
+        );
+
+    pin.addEventListener("click", () => {
+        if (!visible) {
+            tl.play();
+            visible = true;
+        } else {
+            tl.reverse();
+            visible = false;
+        }
+    });
+});
